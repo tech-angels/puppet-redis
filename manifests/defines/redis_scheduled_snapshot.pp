382ca081cd5e6a1f1c3c5721ba6a76751b2ac57c
Gilbert Roulot <gilbert.roulot@tech-angels.com>
2013-01-24 13:05:17 +0100

    Adds archive handling

--- manifests/defines/redis_scheduled_snapshot.pp
+++ manifests/defines/redis_scheduled_snapshot.pp
@@ -2,7 +2,7 @@
 
 define: redis::scheduled_snapshot
 
-This resource installs a cron job that issues the SAVE command to redis so as to make it
+This resource installs a cron job that issues the BGSAVE command to redis so as to make it
 dump its data on disk in /var/lib/redis
 
 Parameters:
@@ -18,18 +18,50 @@ Plus these cron resourse parameters:
 */
 define redis::scheduled_snapshot(
 $user='redis',
+$db_dir,
 $bin,
 $port='6379',
 $hour=undef,
 $minute=undef,
 $month=undef,
 $monthday=undef,
-$weekday=undef
+$weekday=undef,
+$archive_dir=false,
+$max_archive_age='7'
 ) {
+
+
+  # Create archival directory
+  if $archive_dir {
+    if !defined(File[$archive_dir]) {
+      file { $archive_dir:
+        ensure => directory,
+        owner  => $user,
+        mode   => '0700',
+      }
+    }
+
+    # Clean it up of old files
+    exec { "clean up old backups for $name":
+      command => "find ${archive_dir} -mtime +${max_archive_age} -exec rm -f {} \;",
+      user    => $user,
+    }
+
+    $backup_command = "sh -c '[ -f ${db_dir}/dump.rdb ] && mv ${db_dir}/dump.rdb ${archive_dir}/`date +%Y-%m-%d-%H-%M`.rdb ; ${bin}/redis-cli -p ${port} BGSAVE'"
+  } else {
+    $backup_command = "${bin}/redis-cli -p ${port} BGSAVE"
+  }
+  
+
+  # Compute command
+  
+  
+
+
   # Schedule saves
   cron {
     "Scheduled Redis snapshot: $name":
-      command	=> "$bin/redis-cli -p $port SAVE",
+      command	=> $backup_command,
       user	=> $user,
       hour	=> $hour,
       minute	=> $minute,
